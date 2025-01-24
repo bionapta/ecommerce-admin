b11e8b6676ef9a28a3775fabe391d614fecb65ef
bionapta <aaptazaimm@gmail.com>
2025-01-25 01:55:55 +0700

    fix import paths

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -167,7 +167,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                     }
                     onRemove={(url) =>
                       field.onChange([
-                        ...field.value.filter((current) => current.url !== url),
+                        ...field.value.filter((current) => current.url !== url) 
                       ])
                     }
                     value={field.value.map((image) => image.url)}
--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -1,39 +1,39 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-import db from "@/lib/db";
+import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import db from '@/lib/db';
 
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
   try {
-    const { userId } = auth();
+    const { userId } = await auth();
     const body = await req.json();
 
     const { name, price, categoryId, images, isFeatured, isArchived } = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse('Unauthorized', { status: 401 });
     }
 
     if (!name) {
-      return new NextResponse("Nama perlu diinput", { status: 400 });
+      return new NextResponse('Nama perlu diinput', { status: 400 });
     }
 
     if (!images || !images.length) {
-      return new NextResponse("Image perlu diinput", { status: 400 });
+      return new NextResponse('Image perlu diinput', { status: 400 });
     }
 
     if (!price) {
-      return new NextResponse("Harga perlu diinput", { status: 400 });
+      return new NextResponse('Harga perlu diinput', { status: 400 });
     }
 
     if (!categoryId) {
-      return new NextResponse("Kategori perlu diinput", { status: 400 });
+      return new NextResponse('Kategori perlu diinput', { status: 400 });
     }
 
     if (!params.storeId) {
-      return new NextResponse("Store id URL dibutuhkan");
+      return new NextResponse('Store id URL dibutuhkan');
     }
 
     const storeByUserId = await db.store.findFirst({
@@ -44,7 +44,7 @@ export async function POST(
     });
 
     if (!storeByUserId) {
-      return new NextResponse("Unauthorized", { status: 403 });
+      return new NextResponse('Unauthorized', { status: 403 });
     }
 
     const product = await db.product.create({
@@ -65,8 +65,8 @@ export async function POST(
 
     return NextResponse.json(product);
   } catch (error) {
-    console.log("[PRODUCTS_POST]", error);
-    return new NextResponse("Internal error", { status: 500 });
+    console.log('[PRODUCTS_POST]', error);
+    return new NextResponse('Internal error', { status: 500 });
   }
 }
 
@@ -76,11 +76,11 @@ export async function GET(
 ) {
   try {
     const { searchParams } = new URL(req.url);
-    const categoryId = searchParams.get("categoryId") || undefined;
-    const isFeatured = searchParams.get("isFeatured");
+    const categoryId = searchParams.get('categoryId') || undefined;
+    const isFeatured = searchParams.get('isFeatured');
 
     if (!params.storeId) {
-      return new NextResponse("Store id URL dibutuhkan");
+      return new NextResponse('Store id URL dibutuhkan');
     }
 
     const products = await db.product.findMany({
@@ -95,13 +95,13 @@ export async function GET(
         category: true,
       },
       orderBy: {
-        createdAt: "desc",
+        createdAt: 'desc',
       },
     });
 
     return NextResponse.json(products);
   } catch (error) {
-    console.log("[PRODUCTS_GET]", error);
-    return new NextResponse("Internal error", { status: 500 });
+    console.log('[PRODUCTS_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
   }
 }
